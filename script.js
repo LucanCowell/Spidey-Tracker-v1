26988b6b8ef4d2d849ffafd14783d017a1ea3986
Lucan Cowell <lucan.cowell2009@gmail.com>
2026-09-10 08:53:19 +1000

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    
+    // Card Flip
+    
     const flipButtons = document.querySelectorAll(".flip-btn");
 
     flipButtons.forEach(button => {
@@ -7,5 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
             const container = button.closest(".mission-container");
             container.classList.toggle("is-flipped");
         })
+    })
+    
+
+    // Xp Generation
+
+    const xpHeaders = document.querySelectorAll(".xp-reward");
+    xpHeaders.forEach(header => {
+        const randomXP = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
+        header.textContent = `+${randomXP} XP`;
     })
 })
