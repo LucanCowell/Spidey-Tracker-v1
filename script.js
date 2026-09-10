2d1ea443e323e9136c99741df3a043bdc172f857
Lucan Cowell <lucan.cowell2009@gmail.com>
2026-09-10 20:44:21 +1000

    ADDED More mission options

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
     // Possible Missions
     const questPool = [
         { name: "Chemistry", area: "Academics", details: "Complete a Chemistry Practice Test" },
+        { name: "Chemistry", area: "Academics", details: "Review Chemistry Fundamentals for 30 Minutes" },
+        { name: "Chemistry", area: "Academics", details: "Enathalpy and Entropy Questions for 30 Minutes" },
         { name: "Math Advanced", area: "Academics", details: "Complete a Practice Exam targetting weak areas" },
+        { name: "Math Advanced", area: "Academics", details: "Review Key Concepts in Weakest Areas" },
+        { name: "Math Advanced", area: "Academics", details: "Complete a Chapter Review" },
         { name: "Math Extension", area: "Academics", details: "Spend 1 hour reviwing 3D Trigonometry" },
+        { name: "Math Extension", area: "Academics", details: "Spend 30 reviwing the Binomial Theorum" },
+        { name: "Math Extension", area: "Academics", details: "Complete a Practice Test" },
         { name: "English Advanced", area: "Academics", details: "Complete a Short Answer Practice Exam" },
-        { name: "Enterprise Computing", area: "Academics", details: "Spend 1 Hour reviewing the Ubiquity of Interactive Media" },
+        { name: "English Advanced", area: "Academics", details: "Review Quote Flashcards" },
+        { name: "English Advanced", area: "Academics", details: "Write a draft for a 20 mark responce" },
+        { name: "E Computing", area: "Academics", details: "Spend 1 Hour reviewing the Ubiquity of Interactive Media" },
+        { name: "E Computing", area: "Academics", details: "Answer 10 Practice Questions across the syllabus" },
+        { name: "E Computing", area: "Academics", details: "Practice Drawing Network Diagrams" },
         { name: "Business Studies", area: "Academics", details: "Review the Business Report Structure" },
+        { name: "Business Studies", area: "Academics", details: "Answer past papers" },
+        { name: "Business Studies", area: "Academics", details: "Review study notes and then use active recall" },
         { name: "SOR X", area: "Academics", details: "Spend half an hour reviewing Post-45 Content" },
+        { name: "SOR X", area: "Academics", details: "Spend 1 hour reviewing Judaism Content" },
+        { name: "SOR X", area: "Academics", details: "Spend 1 hour reviewing Christianity Content Content" },
         { name: "Calisthenics", area: "Fitness", details: "Spend 10 Minutes practicing Calisthenic Form and Abilities" },
-        { name: "Novel Writing", area: "Queens", details: "Write 500 words on the Exotic Curse" }
+        { name: "Home Gym", area: "Fitness", details: "Complete a variation of the 'CINDY' Workout: (5 Sit ups, 10 Squats, 15 Pushups)" },
+        { name: "Wood Cutting", area: "Fitness", details: "Cut some firewood with the Blockbuster if available" },
+        { name: "Writing", area: "Creative", details: "Write 500 words on the Exotic Curse" },
+        { name: "Music", area: "Creative", details: "Practise a Musical Instrument for 30 minutes" },
+        { name: "Drama", area: "Creative", details: "Review lines for musicals for 30 minutes" },
     ];
 
     const slots = document.querySelectorAll(".mission-slot");
